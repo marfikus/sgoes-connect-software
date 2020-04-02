e471ad1da8853f7acedb4a32578bdae04327cf65
Alexey Zaytsev <alex.rv9rh@gmail.com>
2020-04-02 14:12:27 +0500

    added a button that opens bluetooth settings

--- app/src/main/java/com/example/sgoesconnect/MainActivity.java
+++ app/src/main/java/com/example/sgoesconnect/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.sgoesconnect;
 import androidx.appcompat.app.AppCompatActivity;
 import android.bluetooth.*;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.ResultReceiver;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BT = 0;
+    Button bt_settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +28,29 @@ public class MainActivity extends AppCompatActivity {
 
             if (bluetoothAdapter.isEnabled()) {
                 Toast.makeText(getApplicationContext(), "bluetooth is enabled", Toast.LENGTH_SHORT).show();
+                //String myDeviceName = bluetoothAdapter.getName();
+                //Toast.makeText(getApplicationContext(), myDeviceName, Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(getApplicationContext(), "bluetooth is disabled", Toast.LENGTH_SHORT).show();
+
+                // запрос на включение bluetooth:
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
             }
+
         } else {
             Toast.makeText(getApplicationContext(), "bluetooth adapter is not detected", Toast.LENGTH_SHORT).show();
         }
+
+        bt_settings = (Button) findViewById(R.id.bt_settings);
+        bt_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // вызываем экран настроек bluetooth:
+                startActivity(new Intent(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS));
+            }
+        });
 
 
     }
